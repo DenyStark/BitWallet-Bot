19916ee040883654bc0cffcef9efe8cc5130c6aa
DenyStark <denyskrolevetskyi@gmail.com>
2019-05-19 22:25:17 +0300

    Feat: Utils > Txs

---
+++ src/utils/txs.js
@@ -0,0 +1,15 @@
+const toModel = (txs, timestamp) => {
+  const toModelTxs = txs.map(tx => ({
+    hash: tx.hash,
+    from: tx.from,
+    to: tx.to,
+    value: tx.value,
+    fee: tx.gasPrice * tx.gas,
+    timestamp
+  }));
+  return toModelTxs;
+}
+
+module.exports = {
+  toModel,
+}
