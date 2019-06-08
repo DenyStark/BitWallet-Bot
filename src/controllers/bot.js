f1aacbd017b66513f4063f2fc2c2671dfdb471bd
DenyStark <denyskrolevetskyi@gmail.com>
2019-06-08 23:33:28 +0300

    Ref: Hide link preview on bot message

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -14,6 +14,7 @@ class BotController {
       chat_id: chatId,
       parse_mode: 'Markdown',
       text,
+      disable_web_page_preview: false,
     };
     if (isKeyboard) conf.reply_markup = keyboard;
     /*eslint-enable camelcase */
