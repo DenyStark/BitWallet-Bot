9f45863da59dd47a841444f8c7e8b99b579014e7
DenyStark <denyskrolevetskyi@gmail.com>
2019-06-09 00:11:27 +0300

    Fix: Answer builder error

--- src/utils/answers.js
+++ src/utils/answers.js
@@ -15,21 +15,22 @@ const answers = (type, params = {}) => ({
       `*Your subscription list:*
 
       ${(params.list || []).join('\n')}`,
-  'new-tx':
-      `*New transaction:*
-
-      hash: [${params.tx.hash}](https://ropsten.etherscan.io/tx/${params.tx.hash})
-
-      from: [${params.tx.from}](https://ropsten.etherscan.io/address/${params.tx.from})
-      to: [${params.tx.to}](https://ropsten.etherscan.io/address/${params.tx.to})
-
-      value: ${utils.weiToEth(params.tx.value)} Eth
-      fee: ${utils.weiToEth(params.tx.fee)} Eth
-
-      date: ${utils.toDate(params.tx.date)}`.replace(/ {2}/g, ''),
   'unknown':
       'Unknown command.',
 }[type]);
+
+const newTxAnswer = (params) => `
+  *New transaction:*
+
+  hash: [${params.tx.hash}](https://ropsten.etherscan.io/tx/${params.tx.hash})
+
+  from: [${params.tx.from}](https://ropsten.etherscan.io/address/${params.tx.from})
+  to: [${params.tx.to}](https://ropsten.etherscan.io/address/${params.tx.to})
+
+  value: ${utils.weiToEth(params.tx.value)} Eth
+  fee: ${utils.weiToEth(params.tx.fee)} Eth
+
+  date: ${utils.toDate(params.tx.date)}`.replace(/ {2}/g, '');
 
 /*eslint-disable camelcase */
 const keyboard = JSON.stringify({
@@ -41,5 +42,6 @@ const keyboard = JSON.stringify({
 
 module.exports = {
   answers,
+  newTxAnswer,
   keyboard,
 };
